984964e93eca88146666ff8ff98a9d2db9d72572
Cleverson <SmileYzn@users.noreply.github.com>
2026-03-12 13:03:00 -0300

    Update LXDE installation script with new packages

--- lxde.sh
+++ lxde.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+######################################################
+# FAÇA A INSTALAÇÃO MÍNIMA DO XORG VIA ARCH INSTALLER
+######################################################
+
 # Limpar
 clear
 
@@ -19,19 +23,19 @@ cd /home/$USUARIO
 sudo pacman -Syyu --needed --noconfirm
 
 # Pacotes Base
-sudo pacman -S --needed --noconfirm 7zip alsa-firmware base-devel bash-completion fastfetch fwupd ffmpegthumbnailer git man power-profiles-daemon powertop reflector unace unzip unrar xz zip
+sudo pacman -S --needed --noconfirm 7zip alsa-firmware base-devel bash-completion fastfetch ffmpeg ffmpegthumbnailer git man nano-syntax-highlighting power-profiles-daemon powertop reflector udisks2 unace unzip unrar xz zip
 
 # Pacotes XDG Desktop e User Dirs
-sudo pacman -S --needed --noconfirm xdg-user-dirs xdg-desktop-portal xdg-desktop-portal-gtk xdg-utils
-
-# Bluetoth, CUPS e Touchegg (Pacotes)
-sudo pacman -S --needed --noconfirm blueman bluez cups
-
-# Bluetoth, CUPS e Touchegg (Serviços)
-sudo systemctl enable bluetooth cups
+sudo pacman -S --needed --noconfirm xdg-user-dirs xdg-user-dirs-gtk xdg-desktop-portal xdg-desktop-portal-gtk xdg-utils
 
 # Xorg e Wayland
-sudo pacman -S --needed --noconfirm numlockx xiccd xorg-apps
+sudo pacman -S --needed --noconfirm numlockx xiccd xorg-apps xorg-xinit
+
+# Bluetoth, CUPS e Touchegg (Pacotes)
+sudo pacman -S --needed --noconfirm blueman bluez cups touchegg
+
+# Bluetoth, CUPS e Touchegg (Serviços)
+sudo systemctl enable bluetooth cups touchegg
 
 # NTFS, CIFS, GVFS
 sudo pacman -S --needed --noconfirm cifs-utils ntfs-3g exfat-utils gvfs gvfs-afc gvfs-dnssd gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb gvfs-wsdd
@@ -42,30 +46,28 @@ sudo pacman -S --needed --noconfirm adobe-source-code-pro-fonts adobe-source-san
 # Atualizar o chace de fontes
 sudo fc-cache -f -v
 
+# LXDE + LXDM
+sudo pacman -S --needed --noconfirm iso-codes libnotify lxde lxdm notification-daemon network-manager-applet pavucontrol simple-scan system-config-printer
+
+# Ativar serviço LXDM
+sudo systemctl enable lxdm
+
+# Adwaita
+sudo pacman -S --needed --noconfirm adwaita-cursors adwaita-fonts adwaita-icon-theme adwaita-icon-theme-legacy
+
 # Firefox
 sudo pacman -S --needed --noconfirm firefox firefox-i18n-pt-br
 
 # Pacotes Extras
-sudo pacman -S --needed --noconfirm drawing galculator gcolor3 gnome-screenshot gthumb l3afpad mpv pavucontrol seahorse simple-scan xarchiver
-
-# GStreamer
-sudo pacman -S --needed --noconfirm gstreamer gst-libav gst-plugins-base gst-plugins-good gst-plugins-bad
-
-# LXDE
-sudo pacman -S --needed --noconfirm iso-codes lxde notification-daemon pavucontrol
+sudo pacman -S --needed --noconfirm dconf-editor galculator gcolor3 gigolo gnome-screenshot gparted l3afpad mugshot mpv seahorse xarchiver
 
 # PKGFILE (Retorno de comando não encontrado)
 sudo pacman -S --needed --noconfirm pkgfile
 sudo pkgfile --update
 echo "source /usr/share/doc/pkgfile/command-not-found.bash" >> ~/.bashrc
 
-# Lightdm
-sudo pacman -S --needed --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
-sudo systemctl enable lightdm --force
-
-# Remove LXDM
-sudo systemctl disable lxdm --force
-sudo pacman -Rnsu --noconfirm lxdm
+# Habilitar otification Daemon
+sudo cp /usr/share/applications/notification-daemon.desktop /etc/xdg/autostart/
 
 # YAY (Arch User Repository)
 git clone https://aur.archlinux.org/yay-bin.git
@@ -75,7 +77,7 @@ cd ..
 rm -rf yay-bin
 
 # Limpar pacotes
-sudo pacman -R --noconfirm htop vim vim-runtime
+sudo pacman -Rnscu --noconfirm htop vim vim-runtime
 
 # Limpar dependências
 sudo pacman -Rcs --noconfirm $(pacman -Qdtq)
@@ -111,6 +113,12 @@ xdg-user-dirs-update
 # Remover pastas antigas
 rm -rf Documents Music Pictures Public Templates Videos
 
-# Fim
-exit
+# Nano syntax highlighting
+echo 'include "/usr/share/nano/*.nanorc"' | sudo tee -a /etc/nanorc > /dev/null
+
+# LXDE Panel
+LXPANEL="/Td6WFoAAATm1rRGBMD8BYgQIQEWAAAAAAAAAFlrPSfgCAcC9F0AEYgKCCHMlGgedS3yj4gZAZo+EbH9bnvQzmht+lr00B61+5a14Byz7h8C5yN/5v5WjM1dPql+4E8p2WQTIXQjzztYOVwKj5wlnJQK/Z+bF+zHX4WlEnpThXhODmwwf2j0OdiIVEDJgamlNZQS41KbWPVwIaRLCz6F+krYbTlKi4EwG9v9CGXoNtNlXY4JSL3K9mVVe8iCewTR2QwKcotDwSvX3QAlX9oL1TPfe81kuTpnkR7fgfWyb/sygOIy/92X3GrEXDPOls630ndpS/col/omVTENeTbt60MvuJcLrODxjHQ3gaOwkXsTm5n8RmtFncNrIc20xDLY9VRXUW4UoUVvt5QagbzAvY8dL1WuICGIFrY5gDAU+SRjf2d7MV8l4YxUkAp6bulR7+DGNhXeCl6+mYmtoVDolIigvSCiA8dlYLv+hhd31RWiQ/9XnA2UDW1RGunYnNP/VzR0xzZUgbYRL/M5VHQ+OQ0qm7rcLNZWAE3v7y+9oK0SblAfi1Hiu2ooEsK6vj96sxp82rPJqweUzWVhY6Mbh2ArW9Mee2le0uEULE/VfFPzUPAD0482a3qPhXcVfMIh4bpAEeZ1iXJGtUeu/McLHE+C7Y31DzOSDuUOCbVRupscY9jUG4che6Se5gxLC4UAWzj4m6VreOEBvJAWz9qMiOp2xxEmyGL6/A3s0iw4yAaj+s1aL0GirgiKRxtGw2OS8ub/YKdNiSWkOnCZ1J7FQ4JmYzi8A4LKw/owMwoQCC5b2n/0Rn/SD1fENxzaA5FIWKOUlfLfu3lOgKFLP2YcL+oiC7UZ+VQ00YDJiXVOjZa9b+U8BBs5iVdSnilT/UNj5smpHVxajXOZqVowskVBueeEKMJrciKINBKxN7KRNFYswLDmFwsBXfjps/mg/b89slXd4pn6J2Hz3NwFa4YwCF1m5/gq94hIXHTUbsmxSb7hrcwAqb/FUbvP57HadD5SbJE3ZYt2L/nylHC3UQaiZy89zv4i7BgWJQgAAGN8UMTUZwtjAAGYBogQAAD3iOuJscRn+wIAAAAABFla"
+
+# Restaurar dados do painel
+echo "$LXPANEL" | base64 -d | unxz -c > /home/$USUARIO/.config/lxpanel/LXDE/panels/panel
 
